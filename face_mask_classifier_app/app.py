0a460766edd9f5f9a274f2198c3512e6f503d4f2
Baskar-t <40053093+Baskar-t@users.noreply.github.com>
2021-08-08 13:12:34 +0530

    Update app.py

--- face_mask_classifier_app/app.py
+++ face_mask_classifier_app/app.py
@@ -29,7 +29,7 @@ def mask_image():
     #load mask classifier models
     print("[INFO] loading face mask detector model...")
     #model = load_model(args["model"])
-    model = pickle.load("svm_classifier_model_initial.sav",'rb')
+    model = pickle.load("svm_classifier_model_initial.sav",'r')
 
     embed_list_test=[]
     face_box_list=[]
